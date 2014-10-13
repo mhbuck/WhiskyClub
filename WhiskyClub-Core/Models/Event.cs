f6499492ef5583d3bf4f3d7ad24602e842dfea4e
Michael Buck <mhbuck@gmail.com>
2014-10-13 22:30:42 +0200

    Adding some basic understanding for the VM's
    Maybe need to try unit test before getting a ui?

--- WhiskyClub-Core/Models/Event.cs
+++ WhiskyClub-Core/Models/Event.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace WhiskyClub.Core.Models
 {
-    class Event
+    public class Event
     {
+        public DateTime EventDate { get; set; }
+    }
+
+    public interface IEventApi
+    {
+        Task<List<Event>> GetWhiskyEvents();
+    }
+
+    public class MockEvent : IEventApi
+    {
+        public Task<List<Event>> GetWhiskyEvents()
+        {
+            return Task.FromResult(new List<Event>() 
+            {
+                new Event() { EventDate = DateTime.Now },
+                new Event() { EventDate = DateTime.Now.AddDays(1) },
+                new Event() { EventDate = DateTime.Now.AddDays(-1) },
+            });
+        }
     }
 }
--- WhiskyClub-Core/ViewModels/EventListViewModel.cs
+++ WhiskyClub-Core/ViewModels/EventListViewModel.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using ReactiveUI;
+using Splat;
+using WhiskyClub.Core.Models;
 
 namespace WhiskyClub.Core.ViewModels
 {
-    class EventListViewModel
+    [DataContract]
+    public class EventListViewModel : ReactiveObject, IRoutableViewModel
     {
+        // To Be Reviewed.
+        [IgnoreDataMember]
+        public string UrlPathSegment
+        {
+            get { return "Login"; }
+        }
+
+        [IgnoreDataMember]
+        public IScreen HostScreen { get; protected set; }
+
+        [DataMember]
+        public ReactiveList<Event> Events { get; protected set; }
+
+        [IgnoreDataMember]
+        public ReactiveCommand<List<Event>> LoadEvents { get; protected set; }
+
+        public EventListViewModel(IScreen screen = null)
+        {
+            HostScreen = screen ?? Locator.Current.GetService<IScreen>();
+            Events = new ReactiveList<Event>();
+
+            var api = (IEventApi)new MockEvent();
+
+            // Command to fetch
+            LoadEvents = ReactiveCommand.CreateAsyncTask(async _ => {
+                var results = await api.GetWhiskyEvents();
+                return results;
+            });
+
+            // Clear the VM
+            LoadEvents.Subscribe(i => {
+                Events.Clear();
+                Events.AddRange(i);
+            });
+
+        }
     }
 }
